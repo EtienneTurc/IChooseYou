0e005afa0b508a3ec87df251b219fb42ae6036a3
Etienne Turc <etienne.turc@gmail.com>
2021-07-03 21:13:04 +0200

    Fix randomness issues

--- server/blueprint/chart/service.py
+++ server/blueprint/chart/service.py
@@ -1,4 +1,5 @@
 import io
+import math
 
 from PIL import Image
 
@@ -7,8 +8,6 @@ from server.orm.command import Command
 from server.service.selection.selection import select_from_pick_list
 from server.service.strategy.helper import get_strategy
 
-initial_img_size = 100
-img_final_size = 500
 saturation = 220
 lightness = 200
 
@@ -17,6 +16,10 @@ def create_heat_map(command_name: str, channel_id: str) -> tuple[any, str]:
     command = Command.find_one_by_name_and_chanel(command_name, channel_id)
     item_to_color = map_item_to_color(command.pick_list)
     strategy = get_strategy(command.strategy, command.weight_list)
+
+    n = len(command.pick_list)
+    initial_img_size = max(100 / n, math.ceil(100 / n)) * n
+    img_final_size = 5 * initial_img_size
 
     img = Image.new("HSV", (initial_img_size, initial_img_size), "black")
     pixels = img.load()
--- server/service/command/randomness.py
+++ server/service/command/randomness.py
@@ -1,8 +1,22 @@
+from datetime import datetime
+
 from flask import current_app
 
+from server.orm.command import Command
 from server.service.command.args import Arg
 from server.service.command.base_command import BaseCommand, addHelp
 from server.service.slack.message import Message, MessageStatus, MessageVisibility
+
+
+def construct_url(base_url: str, params: dict[str, str]) -> str:
+    params_url = ""
+    for param_key in params:
+        params_url += f"&{param_key}={params[param_key]}"
+
+    if params_url:
+        params_url = f"?{params_url[1:]}"
+
+    return base_url + params_url
 
 
 class RandomnessCommand(BaseCommand):
@@ -33,9 +47,15 @@ class RandomnessCommand(BaseCommand):
     @addHelp
     def exec(self, *args, **kwargs):
         command_name = self.options.get("command_name")
+        Command.find_one_by_name_and_chanel(command_name, channel_id=self.channel_id)
 
         api_url = current_app.config["API_URL"]
-        image_url = f"{api_url}/chart/heat-map?command_name={command_name}&channel_id={self.channel_id}"  # noqa E501
+        params = {
+            "command_name": command_name,
+            "channel_id": self.channel_id,
+            "t": datetime.now().timestamp(),
+        }
+        image_url = construct_url(f"{api_url}/chart/heat-map", params)
         message = "*Randomness of the command*\n"
         message += "Each color represents a different item of the pick list"
         return Message(
