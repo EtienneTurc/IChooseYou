29a85ce7d8844a1b08173fa048b2808902b7195d
Etienne Turc <etienne.turc@gmail.com>
2022-04-04 23:01:59 +0200

    Test with extension

--- cython_build.py
+++ cython_build.py
@@ -2,15 +2,18 @@ import os
 
 import numpy as np
 from Cython.Build import cythonize
-from setuptools import setup
+from setuptools import Extension, setup
 
 build_html_file = True if os.environ.get("BUILD_HTML_FILE") else False
 
 setup(
     ext_modules=cythonize(
-        "server/service/wheel/cython/create_frame.pyx",
+        Extension(
+            "create_frame",
+            ["server/service/wheel/cython/create_frame.pyx"],
+            include_dirs=[np.get_include()],
+        ),
         annotate=build_html_file,
         language_level="3",
     ),
-    include_dirs=[np.get_include()],
 )
