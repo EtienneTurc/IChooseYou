981011c6014850add4ad4b550b8ab39a90f26dc7
Etienne Turc <etienne.turc@gmail.com>
2022-04-04 22:48:29 +0200

    Specify language level

--- cython_build.py
+++ cython_build.py
@@ -8,7 +8,9 @@ build_html_file = True if os.environ.get("BUILD_HTML_FILE") else False
 
 setup(
     ext_modules=cythonize(
-        "server/service/wheel/cython/create_frame.pyx", annotate=build_html_file
+        "server/service/wheel/cython/create_frame.pyx",
+        annotate=build_html_file,
+        language_level="3",
     ),
     include_dirs=[np.get_include()],
 )
